65c695ec699e1f2ae283fd5c9fac6ee2099b39c3
Alexrocchi <alexandra.rochhi@flyingheads.com>
2025-09-07 20:23:01 +0200

    feature: init proj

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NextJS
+
+## Project Setup
+1. Create Next app
+ npx create-next-app
+
+Command to run the app: `npm run dev`
---
+++ todosapp/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello world!</h1>
+      <h2 className="font-luckiest-guy">Welcome to Next.js!</h2>
+    </main>
+  );
+}
