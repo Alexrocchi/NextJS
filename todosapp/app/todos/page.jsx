246751430e27bdc658f033b6141d9711d4d0168a
Alexrocchi <alexandra.rochhi@flyingheads.com>
2025-09-15 20:37:47 +0200

    feature: add DeleteTodo fn

--- todosapp/app/todos/page.jsx
+++ todosapp/app/todos/page.jsx
@@ -1,9 +1,48 @@
-function TodosPage() {
-  return (
-    <div>
-      Todos Page
-    </div>
-  )
+import DeleteTodo from "@/components/DeleteTodo";
+
+async function TodosPage() {
+    
+    const response = await fetch('http://localhost:3000/api/todos', { 
+        // If we not use 'no-store', Next.js will cache the response by default and we won't see updates. Whith this attribute we're telling Next.js to always fetch the latest data.
+        cache: 'no-store' 
+        
+        // If we want to chache the data, we can use 'next', Next.js will revalidate the data every seconds we specify
+        /* next : { revalidate: 10 } // Revalidate the data every 10 seconds */ 
+    });
+    const data = await response.json();
+    
+    console.log(data);
+    
+    
+    return (
+        <section className="mt-24 w-full h-full flex justify-center">
+            <table className="min-w-max bg-white border border-gray-200 rounded-lg overflow-hidden">
+                <thead className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
+                    <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
+                        <th className="py-3 px-6 text-left">ID</th>
+                        <th className="py-3 px-6 text-left">Name</th>
+                        <th className="py-3 px-6 text-left">Action</th>
+                    </tr>
+                </thead>
+                <tbody className="text-gray-600 text-sm font-light">
+                    {data.map((todo) => (
+                        <tr key={todo.id} className="border-b border-gray-200 hover:bg-gray-100">
+                            <td className="py-3 px-6">
+                                {todo.id}
+                            </td>
+                            <td className="py-3 px-6">
+                                {todo.name}
+                            </td>
+                            <td className="py-3 px-6">
+                                {/* A server-side doen't know when to re render, so we can't use an event handler like onClick. We need to use a form to send a request to the server */}
+                                <DeleteTodo id={todo.id} />
+                            </td>                            
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </section>
+    )
 }
 
 export default TodosPage
---
+++ todosapp/components/DeleteTodo.jsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
+function DeleteTodo( { id } ) {
+
+    // To refresh the page after deleting a Todos
+    const router = useRouter();
+
+    async function handleDelete() {        
+        const response = await fetch('http://localhost:3000/api/todos', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id })
+        })
+
+        if(response.ok) {
+            // Refresh the page to show the updated list of todos
+            router.refresh();
+        }else{
+            console.error('Failed to delete the todo');
+        }
+    }
+
+  return (
+    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={handleDelete}>Delete</button>
+  )
+}
+
+export default DeleteTodo
