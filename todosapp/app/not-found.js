c22a4307892003188cec7ae9f275a1041b70be29
Alexrocchi <alexandra.rochhi@flyingheads.com>
2025-09-17 20:32:48 +0200

    fix: scape text

--- todosapp/app/not-found.js
+++ todosapp/app/not-found.js
@@ -3,7 +3,7 @@ function notFoundPage() {
     <div className="flex justify-center items-center h-screen flex-col gap-4">
       <p className="text-9xl font-luckiest-guy">404</p>
       <p>Page not found</p>
-      <small>This page is an example of a custom 404 page. If you're trying to visit a todos with an id greater than 3, you'll see this message.</small>
+      <small>This page is an example of a custom 404 page. If you&apos;re trying to visit a todos with an id greater than 3, you&apos;ll see this message.</small>
     </div>
   )
 }
